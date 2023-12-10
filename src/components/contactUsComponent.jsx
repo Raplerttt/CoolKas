670d34fbf194659c7c35ba14e64cc5fe79da59cb
Azzahrasth <azzahrasth@upi.edu>
2023-12-10 21:09:35 +0700

    connect contact us ke email

--- src/components/contactUsComponent.jsx
+++ src/components/contactUsComponent.jsx
@@ -1,9 +1,38 @@
 import "../style/contactUs.css";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 function ContactUs() {
+  const formContact = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_2cugkzq",
+        "template_emwbjas",
+        formContact.current,
+        "RZWMKgwWAakxNqYLd"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          window.alert("Pesan telah berhasil di kirim!");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <div>
       <div className="row contact">
-        <div className="col-md-7 form-contact">
+        <form
+          ref={formContact}
+          onSubmit={sendEmail}
+          className="col-md-7 form-contact"
+        >
           <p>
             Jangan ragu untuk menghubungi kami kapan saja. <br /> Kami akan
             segera menghubungi Anda !
@@ -17,6 +46,7 @@ function ContactUs() {
               className="form-control"
               id="formGroupExampleInput"
               placeholder="Masukan nama Anda"
+              name="user_name"
             />
           </div>
           <div className="mb-3">
@@ -28,24 +58,31 @@ function ContactUs() {
               className="form-control"
               id="formGroupExampleInput2"
               placeholder="Masukan email Anda"
+              name="user_email"
             />
           </div>
 
-          <div className="mb-3">
+          <div className="mb-4">
             <label for="exampleFormControlTextarea1" className="form-label">
               Pesan
             </label>
             <textarea
               className="form-control"
               id="exampleFormControlTextarea1"
-              rows="4"
+              rows="2"
               placeholder="Masukan pesan Anda"
+              name="message"
             ></textarea>
           </div>
           <div className=" text-center">
-            <button className="btn button-submit mx-auto">Kirim</button>
+            <button
+              type="submit"
+              className="btn button-submit mx-auto rounded-4"
+            >
+              Kirim
+            </button>
           </div>
-        </div>
+        </form>
 
         <div className="col-md-5 rounded-4 info-container">
           <h2>Info</h2>
