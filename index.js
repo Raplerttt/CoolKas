7176b8d16a39567a39ce0b0e1f9fc6fa1d889967
Raplerttt <142213484+Raplerttt@users.noreply.github.com>
2023-12-18 16:40:22 +0700

    final

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const bodyParser = require("body-parser");
 const userRoutes = require("./src/User/user.controllers");
 const modulRoutes = require("./src/Modul/modulControllers");
@@ -8,7 +9,11 @@ const penyimpananRoutes = require("./src/Penyimpanan/penyimpananControllers");
 const app = express();
 const PORT = 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://mycoolkas.my.id',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 // middleware
 app.use(bodyParser.json());
 app.use("/", userRoutes);
